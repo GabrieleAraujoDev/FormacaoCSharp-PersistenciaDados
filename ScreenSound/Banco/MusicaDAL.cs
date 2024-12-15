4eb34037ce1f1b2fcdbc3ef7943678dd3070eb84
Gabriele Araújo <gabriele.carla.ca@gmail.com>
2024-12-15 10:28:09 -0300

    Musicas

---
+++ ScreenSound/Banco/MusicaDAL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Banco
+{
+    internal class MusicaDAL
+    {
+        private readonly ScreenSoundContext context;
+
+        public MusicaDAL(ScreenSoundContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Musica> Listar()
+        {
+            return context.Musicas.ToList();
+        }
+
+        public void Adicionar(Musica Musica)
+        {
+            context.Musicas.Add(Musica);
+            context.SaveChanges();
+        }
+
+        public void Atualizar(Musica Musica)
+        {
+            context.Musicas.Update(Musica);
+            context.SaveChanges();
+        }
+
+        public void Deletar(Musica Musica)
+        {
+            context.Musicas.Remove(Musica);
+            context.SaveChanges();
+        }
+    }
+}
--- ScreenSound/obj/Debug/net6.0/ScreenSound.AssemblyInfo.cs
+++ ScreenSound/obj/Debug/net6.0/ScreenSound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cd6a4ec8741a3f6958d4dca05575eed01134082")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faf86fdd8e0cccb94753ae543875ce963afe9c38")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
